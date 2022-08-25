cca335dee6b08bf81fb178569eb3c6ff8d15330d
EviSONG <evisong@live.com>
2022-08-25 17:32:33 +0800

    ch-03: 列表数据转为state

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const todoList = [
-  { title: '开发任务-1', status: '22-05-22 18:15' },
-  { title: '开发任务-3', status: '22-05-22 18:15' },
-  { title: '开发任务-5', status: '22-05-22 18:15' },
-  { title: '测试任务-3', status: '22-05-22 18:15' }
-];
-const ongoingList = [
-  { title: '开发任务-4', status: '22-05-22 18:15' },
-  { title: '开发任务-6', status: '22-05-22 18:15' },
-  { title: '测试任务-2', status: '22-05-22 18:15' }
-];
-const doneList = [
-  { title: '开发任务-2', status: '22-05-22 18:15' },
-  { title: '测试任务-1', status: '22-05-22 18:15' }
-];
-
 const KanbanCard = ({ title, status }) => {
   return (
     <li className="kanban-card">
@@ -51,11 +35,29 @@ const KanbanNewCard = ({ onSubmit }) => {
 
 function App() {
   const [showAdd, setShowAdd] = useState(false);
+  const [todoList, setTodoList] = useState([
+    { title: '开发任务-1', status: '22-05-22 18:15' },
+    { title: '开发任务-3', status: '22-05-22 18:15' },
+    { title: '开发任务-5', status: '22-05-22 18:15' },
+    { title: '测试任务-3', status: '22-05-22 18:15' }
+  ]);
+  const [ongoingList, setOngoingList ] = useState([
+    { title: '开发任务-4', status: '2022-05-22 18:15' },
+    { title: '开发任务-6', status: '2022-06-22 18:15' },
+    { title: '测试任务-2', status: '2022-07-22 18:15' }
+  ]);
+  const [doneList, setDoneList ] = useState([
+    { title: '开发任务-2', status: '2022-06-24 18:15' },
+    { title: '测试任务-1', status: '2022-07-03 18:15' }
+  ]);
   const handleAdd = (evt) => {
     setShowAdd(true);
   };
   const handleSubmit = (title) => {
-    todoList.unshift({ title, status: new Date().toDateString() });
+    setTodoList(currentTodoList => [
+      { title, status: new Date().toDateString() },
+      ...currentTodoList
+    ]);
     setShowAdd(false);
   };
 

ba9b35638abbf2dcbef1b5799ca9f963aba3c6fc
cjh <cjh521cjh@163.com>
2024-06-19 21:06:51 +0800

    引入stylelint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(<App />);
+// const container = document.getElementById("root");
+// const root = createRoot(container!);
+// root.render(<App />);
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+
+import App from "./App.tsx";
+import "./index.scss";
+
+function render(props) {
+  const container = document.getElementById("root") as HTMLElement;
+  // Concurrent mode
+  const root = ReactDOMClient.createRoot(container);
+  root.render(<App {...props} />);
+  // requestIdleCallback(callback);
+}
+
+render({});
